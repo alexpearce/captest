09d57d18ae823ea9c0860e245ce9932888f0cc73
Alex Pearce <alex@alexpearce.me>
2012-06-18 18:07:52 +0100

    Add config symlink task.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@
 # http://pemberthy.blogspot.co.uk/2009/02/deploying-sinatra-applications-with.html
 # https://github.com/rubenfonseca/sinatra-capistrano-workshop
 # http://henriksjokvist.net/archive/2012/2/deploying-with-rbenv-and-capistrano/
+# http://norbyit.se/blog/2011/01/code-deployment/
 
 require "bundler/capistrano"
 set :bundle_flags, "--deployment --quiet"
@@ -12,8 +13,12 @@ set :default_environment, {
 
 set :application, "captest"
 set :user, "deploy"
-set :use_sudo, false
+set :use_sudo, true
 
+# Required for sudo password prompt
+default_run_options[:pty] = true
+
+# Allows passing on our local public keys
 ssh_options[:forward_agent] = true
 
 set :scm, :git
@@ -25,16 +30,23 @@ set :deploy_via, :remote_cache
 role :app, "31.193.143.153"
 role :web, "31.193.143.153"
 
+after "deploy:cold" do
+  deploy.symlink_config
+end
+
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
+  desc "Not starting as we're running passenger."
   task :start do
   end
 
+  desc "Not stopping as we're running passenger."
   task :stop do
   end
 
-  task :restart, :roles => :app, :except => { :no_release => true } do
+  desc "Restart the app."
+  task :restart, roles: :app, except: { :no_release => true } do
     run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 
@@ -42,5 +54,10 @@ namespace :deploy do
   task :cold do
     deploy.update
     deploy.start
+  end
+
+  desc "Link the server config to nginx."
+  task :symlink_config, roles: :app do
+    run "#{sudo} ln -nfs #{deploy_to}/current/config/nginx.server /etc/nginx/sites_enabled/#{application}"
   end
 end
