73e0accd231512cc33e05880ca803ce7f4de05ac
Alex Pearce <alex@alexpearce.me>
2012-06-18 18:20:34 +0100

    Comments.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,16 +4,21 @@
 # http://henriksjokvist.net/archive/2012/2/deploying-with-rbenv-and-capistrano/
 # http://norbyit.se/blog/2011/01/code-deployment/
 
+# Use Bundler
 require "bundler/capistrano"
 set :bundle_flags, "--deployment --quiet"
 
+# Load rbenv
 set :default_environment, {
   "PATH" => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
 }
 
+# Application name
 set :application, "captest"
+# User to deploy as
 set :user, "deploy"
-set :use_sudo, true
+# No need for sudo. Can use #{sudo} if we explicitly need it
+set :use_sudo, false
 
 # Required for sudo password prompt
 default_run_options[:pty] = true
@@ -21,17 +26,20 @@ default_run_options[:pty] = true
 # Allows passing on our local public keys
 ssh_options[:forward_agent] = true
 
+# Use git, set the repo
 set :scm, :git
 set :repository,  "git@github.com:alexpearce/captest.git"
 
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 
+# Where is the app server?
 role :app, "31.193.143.153"
-role :web, "31.193.143.153"
 
+# After an initial (cold) deploy, symlink the app and restart nginx
 after "deploy:cold" do
-  deploy.symlink_config
+  admin.symlink_config
+  admin.nginx_restart
 end
 
 
@@ -55,9 +63,21 @@ namespace :deploy do
     deploy.update
     deploy.start
   end
+end
 
-  desc "Link the server config to nginx."
+namespace :admin do
+    desc "Link the server config to nginx."
   task :symlink_config, roles: :app do
     run "#{sudo} ln -nfs #{deploy_to}/current/config/nginx.server /etc/nginx/sites_enabled/#{application}"
+  end
+
+  desc "Unlink the server config."
+  task :unlink_config, roles: :app do
+    run "#{sudo} rm /etc/nginx/sites_enabled/#{application}"
+  end
+
+  desc "Restart nginx."
+  task :nginx_restart, roles: :app do
+    run "#{sudo} /etc/init.d/nginx restart"
   end
 end
